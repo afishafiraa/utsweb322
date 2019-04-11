5116ed19e4d20352740c1fca9ef2b9d1c27d785b
Shafira Ananda <afishafiraa@gmail.com>
2019-04-11 08:25:28 +0700

    first

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const theme = [
+    {
+        color   : 'putih',
+        text    : 'theme light'
+    },
+    {
+        color   : 'grey',
+        text    : 'theme dark'
+    }
+]
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
